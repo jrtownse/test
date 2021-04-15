b21c144d16bb15124fd4db9002e61f2bc597d7d0
jrtownse <jonathan@vandersend.com>
2021-04-15 16:00:07 -0400

    init

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
+import os
 
-import sys
 import requests
 
-#print(sys.version)
-#print(sys.executable)
+ print(sys.version)
+ print(sys.executable)
 
 def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return (greeting)
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
 
-r = requests.get('https://coreyms.com')
+
+r = requests.get("https://coreyms.com")
 print(r.status_code)
+#testsync
